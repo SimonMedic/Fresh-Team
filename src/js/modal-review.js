79915da82db186e2481f82996edfe65b55f6b84a
SimonMedic <szymonpaluch@hotmail.com>
2023-11-23 14:15:25 +0100

    Update modal-review.js

--- src/js/modal-review.js
+++ src/js/modal-review.js
@@ -1,14 +1,14 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[review-modal-open]"),
-      closeModalBtn: document.querySelector("[review-modal-close]"),
-      modal: document.querySelector("[review-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelector('[modal-review-open]'),
+    closeModalBtn: document.querySelector('[modal-review-close]'),
+    modal: document.querySelector('[modal-review]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
