ed972f0f8e63b64ed86a32d8945abea525d3d2b4
SimonMedic <145046861+SimonMedic@users.noreply.github.com>
2023-11-23 14:31:36 +0000

    Deploying to gh-pages from @ SimonMedic/Fresh-Team@b68edc9a4de49c1581eaac4e321d18b067859289 🚀

--- index.f1614a1c.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[modal-subscribe-open]"),closeModalBtn:document.querySelector("[modal-subscribe-close]"),modal:document.querySelector("[modal-subscribe]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.f1614a1c.js.map
