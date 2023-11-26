4f83a55e33f58e05bbba0132179c128bb8b31450
SimonMedic <145046861+SimonMedic@users.noreply.github.com>
2023-11-26 23:43:01 +0000

    Deploying to gh-pages from @ SimonMedic/Fresh-Team@07fb17a98fd065f1ee7187f01da80feb3bd494c3 🚀

---
+++ index.8ba0c9aa.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){new Splide("#splide",{type:"slide",perPage:3,gap:28,pagination:!1,arrows:!1}).mount()}));
+//# sourceMappingURL=index.8ba0c9aa.js.map
